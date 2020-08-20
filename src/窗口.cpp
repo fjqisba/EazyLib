535fa8cea705cd5f1bd6f48b1ab1a9192d4fb240
fjqisba <fjqisba@sohu.com>
2020-08-20 23:38:31 +0800

    content update

--- src/窗口.cpp
+++ src/窗口.cpp
@@ -1,14 +1,38 @@
 #include "窗口.h"
 
+std::string 窗口_取标题(HWND 窗口句柄)
+{
+	std::string ret;
+	unsigned int BufLen = GetWindowTextLengthA(窗口句柄) + 1;
+	char* pBuffer = new char[BufLen];
+	if (!pBuffer)
+	{
+		return ret;
+	}
+	memset(pBuffer, 0, BufLen);
+	GetWindowTextA(窗口句柄, pBuffer, BufLen);
+	ret = pBuffer;
+	delete[] pBuffer;
+	return ret;
+}
+
+std::string 窗口_取类名(HWND 窗口句柄)
+{
+	std::string ret;
+	char* pBuffer = new char[50];
+	if (!pBuffer)
+	{
+		return ret;
+	}
+	memset(pBuffer, 0, 50);
+	GetClassNameA(窗口句柄, pBuffer, 50);
+	ret = pBuffer;
+	delete[] pBuffer;
+	return ret;
+}
+
 void 窗口_投递文本(HWND 窗口句柄, std::string& 文本内容)
 {
-	//这段代码尚处于测试阶段
-	DWORD StartTime = GetTickCount();
-	while (GetTickCount() < StartTime + 100)
-	{
-		SetForegroundWindow(窗口句柄);
-	}
-	
 	for (unsigned int n = 0; n < 文本内容.length(); n++)
 	{
 		PostMessageA(窗口句柄, WM_CHAR, 文本内容[n], 0);
@@ -18,13 +42,6 @@ void 窗口_投递文本(HWND 窗口句柄, std::string& 文本内容)
 
 void 窗口_投递文本(HWND 窗口句柄, std::string 文本内容)
 {
-	//这段代码尚处于测试阶段
-	DWORD StartTime = GetTickCount();
-	while (GetTickCount() < StartTime + 100)
-	{
-		SetForegroundWindow(窗口句柄);
-	}
-
 	for (unsigned int n = 0; n < 文本内容.length(); n++)
 	{
 		PostMessageA(窗口句柄, WM_CHAR, 文本内容[n], 0);
